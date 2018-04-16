fd827b91730f2c392f62d1160991a2b3fe6c70b9
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2018-04-16 22:08:23 +0300

    [DSCP-59] Add comment with concern about serialization

--- src/Disciplina/Crypto/Hash/Impl.hs
+++ src/Disciplina/Crypto/Hash/Impl.hs
@@ -15,6 +15,13 @@ import Disciplina.Crypto.Hash.Cryptonite (CryptoniteFunc (..))
 
 type Hash a = AbstractHash (CryptoniteFunc Blake2sp_256) a
 
+-- TODO: These functions work for any 'ByteArrayAccess' instance.
+-- But datatypes which have no such instance must be serialized before hashing.
+-- Serialization is expensive (at least because of memory allocation and copying),
+-- and we'd like to avoid it for datatypes with 'ByteArrayAccess'.
+-- At the same time, calling 'serialise' manually every time for hashing some
+-- non-trivial types is terrible. How can we do 'serialise' inside 'hash' function
+-- for complex types, and omit it for types with 'ByteArrayAccess'?
 hash :: forall a. ByteArrayAccess a => a -> Hash a
 hash = abstractHash
 
