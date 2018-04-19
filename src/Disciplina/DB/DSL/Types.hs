fe3027321b0d0b87946d7db1d280288d8c2c7100
Joakim Goldkuhl <joakim.goldkuhl@serokell.io>
2018-04-19 17:59:13 +0000

    [DSCP-66] Simplifiy DSL a bit

--- src/Disciplina/DB/DSL/Types.hs
+++ src/Disciplina/DB/DSL/Types.hs
@@ -23,39 +23,40 @@ infixr 4 :& -- todo, chose infixr
 data a :== b = a :== b
 infixr 3 :== -- todo, chose infixr
 
-data Filters a = SubjectId a
-               | Done
+data (:~>) = Text :~> Text
+infixr 5 :~> -- todo, chose infixr
+
+data SubjectId = SubjectId
+
+data HashEq = TxHash
+            | ObjHash
 
 data Grade = Grade
 
 data WHERE = WHERE
 
-data WHEREStm a = HashEq Text
-                | FilterCond a
-
-data Query a = SELECT Entity WHERE (WHEREStm a)
-
+data Query a = SELECT Entity WHERE a
 
 -- query construction
 
 -- Precise tx queries:
 -- given <tx_hash>, I want to be able to get { <tx> | hash(<tx>) = <tx_hash> }
-q1 txHash = SELECT Tx WHERE (HashEq txHash)
+q1 txHash = SELECT Tx WHERE (TxHash :== txHash)
 
 -- Precise object queries:
 -- given <obj_hash>, I want to be able to get { <obj> | hash(<obj>) = <obj_hash> }
-q2 objHash = SELECT Obj WHERE (HashEq objHash)
+q2 objHash = SELECT Obj WHERE (ObjHash :== objHash)
 
 -- Range queries:
 -- given <ordered_tx_param>,
 -- I want to be able to get { [<tx>] | P(<ordered_tx_param>) }, where P is a system of (in)equalities
-q3 txParams = SELECT Txs WHERE (FilterCond (SubjectId :== 1 :& Grade :>= B :& Grade :== C))
+q3 txParams = SELECT Txs WHERE (SubjectId :== 1 :& Grade :>= B :& Grade :== C)
 
 -- Subject queries:
 -- given <subject_name>,
 -- I want to be able to get { [<tx>] | subj(<tx>) = S, <subject_name> ~> S },
 -- where "A ~> B" denotes "there's a path in ATG from A to B"
-q4 = undefined
+q4 txParams subjName = SELECT Txs WHERE (SubjectId :== 1 :& Grade :>= B :& subjName :~> "subjectB")
 
 -- Range queries and subject queries should be able to be combined by OR and AND operators.
 -- NOT operator isn't going to be supported (most likely).
