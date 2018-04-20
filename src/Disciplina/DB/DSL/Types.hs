f20b2fed21262e702a32ff2f474a8e414a7cbe80
Joakim Goldkuhl <joakim.goldkuhl@serokell.io>
2018-04-20 17:45:59 +0000

    [DSCP-66] Clean up DSL types

--- src/Disciplina/DB/DSL/Types.hs
+++ src/Disciplina/DB/DSL/Types.hs
@@ -7,36 +7,36 @@ module Disciplina.DB.DSL.Types
        ) where
 
 import Universum
-import Disciplina.Core (Grade(..))
+import qualified Disciplina.Core as Core (Grade(..))
 
-data EntityEq = N Int
-              | TxHash Text
 
 data Entity = Tx
             | Txs
             | Obj
 
-data SubjectId = SubjectId
+data FilterEntity = TxHashF
+                  | ObjHashF
+                  | Hash Text
+                  | SubjectIdF
+                  | SubjectId Int
+                  | GradeF
+                  | Grade Core.Grade
 
-data HashEq =  ObjHash
-
-data Grade = Grade
+data ATGNode = ATGNode Text
 
 data WHERE = WHERE
 
 data FilterExpr where
-   Done  :: FilterExpr
-   (:==) :: Entity -> EntityEq -> FilterExpr -> FilterExpr
-   (:>=) :: Entity -> EntityEq -> FilterExpr -> FilterExpr
+   (:==) :: FilterEntity -> FilterEntity -> FilterExpr
+   (:>=) :: FilterEntity -> FilterEntity -> FilterExpr
    (:&)  :: FilterExpr -> FilterExpr -> FilterExpr
-   (:~>) :: FilterExpr -> FilterExpr -> FilterExpr
+   (:~>) :: ATGNode -> ATGNode -> FilterExpr
 
 infixr 5 :>= -- todo, chose infixr
 infixr 4 :& -- todo, chose infixr
---infixr 3 :== -- todo, chose infixr
+infixr 3 :== -- todo, chose infixr
 infixr 5 :~> -- todo, chose infixr
 
---infixr 3 :=== -- todo, chose infixr
 
 data Query = SELECT Entity WHERE FilterExpr
 
@@ -44,24 +44,25 @@ data Query = SELECT Entity WHERE FilterExpr
 
 -- Precise tx queries:
 -- given <tx_hash>, I want to be able to get { <tx> | hash(<tx>) = <tx_hash> }
-q1 txHash = SELECT Tx WHERE ((Tx :== (TxHash "abc")) Done)
+q1 txHash = SELECT Tx WHERE (TxHashF :== (Hash "abc"))
 
 -- Precise object queries:
 -- given <obj_hash>, I want to be able to get { <obj> | hash(<obj>) = <obj_hash> }
---q2 objHash = SELECT Obj WHERE (ObjHash :== objHash)
+q2 objHash = SELECT Obj WHERE ((ObjHashF :== (Hash objHash)))
 
 -- Range queries:
 -- given <ordered_tx_param>,
 -- I want to be able to get { [<tx>] | P(<ordered_tx_param>) }, where P is a system of (in)equalities
-q3 txParams = SELECT Txs WHERE (SubjectId :== 1 :& Grade :>= B :& Grade :== C)
+q3 txParams = SELECT Txs WHERE ((SubjectIdF :== (SubjectId 1))
+                             :& (GradeF :>= Grade Core.B))
 
 -- Subject queries:
 -- given <subject_name>,
 -- I want to be able to get { [<tx>] | subj(<tx>) = S, <subject_name> ~> S },
 -- where "A ~> B" denotes "there's a path in ATG from A to B"
---q4 txParams subjName = SELECT Txs WHERE (SubjectId :== 1 :& Grade :>= B :& subjName :~> "subjectB")
+q4 txParams subjName = SELECT Txs WHERE ((SubjectIdF :== (SubjectId 1))
+                                      :& (GradeF :>= Grade Core.B)
+                                      :& (ATGNode subjName :~> ATGNode "subjectB"))
 
 -- Range queries and subject queries should be able to be combined by OR and AND operators.
 -- NOT operator isn't going to be supported (most likely).
-
-
