70ffd9e538efd2b94caefeb37575a417bdc02f2f
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2018-04-25 21:20:53 +0300

    [DSCP-71] `Serialise` packing for messages

--- src/Disciplina/Messages.hs
+++ src/Disciplina/Messages.hs
@@ -2,11 +2,13 @@ module Disciplina.Messages where
 
 import Universum
 
-import Codec.Serialise (Serialise)
+import Codec.Serialise (IDecode (..), Serialise, deserialiseIncremental, serialise)
+import Control.Monad.ST (RealWorld, ST)
 import qualified Data.ByteString as BS
 import Data.Data (Data)
-import Node (Message, formatMessage, messageCode)
-import Node.Message.Binary (BinaryP, binaryPacking)
+import Node.Message.Class (Message (..), PackingType (..), Serializable (..))
+import qualified Node.Message.Class as Msg
+import qualified Node.Message.Decoder as Msg
 
 -- | Type for messages from the workers to the witnessListeners.
 data PingBlk = PingBlk
@@ -38,5 +40,25 @@ deriving instance Generic PongTx
 deriving instance Show PongTx
 instance Serialise PongTx
 
-type Packing = BinaryP
+-- | Pack messages using 'Codec.Serialise'
+data SerialiseP
+
+instance PackingType SerialiseP where
+    type PackM SerialiseP = Identity
+    type UnpackM SerialiseP = IO
+
+fromIDecode :: IDecode RealWorld a -> Msg.DecoderStep (UnpackM SerialiseP) a
+fromIDecode (Done bs bo res) = Msg.Done bs bo res
+fromIDecode (Fail bs bo err) = Msg.Fail bs bo (show err)
+fromIDecode (Partial f)      = Msg.Partial $ mkDecoder . f
+
+mkDecoder :: ST RealWorld (IDecode RealWorld a)
+          -> Msg.Decoder (UnpackM SerialiseP) a
+mkDecoder = Msg.Decoder . fmap fromIDecode . stToIO
+
+instance Serialise a => Serializable SerialiseP a where
+    packMsg _ = pure . serialise
+    unpackMsg _ = mkDecoder deserialiseIncremental
+
+type Packing = SerialiseP
 
