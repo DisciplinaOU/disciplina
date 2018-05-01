341a0ede625799dddd080992cb2fb0809819bdcc
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2018-05-01 20:23:14 +0300

    Fix CI (#11)

--- default.nix
+++ default.nix
@@ -2,7 +2,6 @@ let
   nixpkgs = import "${overlay}/nixpkgs.nix";
   overlay = builtins.fetchGit {
     url = "ssh://git@github.com:/serokell/serokell-ops.git";
-    rev = "e6e22f100da2834685246127e064dfa5a321c9df";
   };
 in
 
@@ -11,8 +10,10 @@ with nixpkgs;
 buildStack {
   package = "disciplina";
   src = lib.cleanSource ./.;
+  ghc = pkgs.haskell.compiler.ghc822;
 
   overrides = final: previous: {
     rocksdb-haskell = dependCabal previous.rocksdb-haskell [ rocksdb ];
+    cardano-sl-networking = haskell.lib.appendConfigureFlag previous.cardano-sl-networking "--ghc-option=-fno-warn-redundant-constraints";
   };
 }
