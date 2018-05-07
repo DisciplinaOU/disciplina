d1c5f09333036da3b7eac2f7a6ead648f4448f69
Joakim Goldkuhl <joakim.goldkuhl@serokell.io>
2018-05-07 19:34:44 +0000

    [DSCP-67] Add SimpleTxDB DSL interpreters

--- src/Disciplina/DB/DSL/SimpleTxDB.hs
+++ src/Disciplina/DB/DSL/SimpleTxDB.hs
@@ -2,11 +2,11 @@ module Disciplina.DB.DSL.SimpleTxDB () where
 
 import Universum
 
+import Codec.Serialise (Serialise(..))
+import Control.Lens (filtered, traversed)
 import Crypto.Error (CryptoFailable(..))
-import Codec.Serialise (serialise, deserialise)
+
 import Disciplina.Crypto.Signing.Class (AbstractPK(..), AbstractSK(..))
-import Data.ByteArray (ByteArrayAccess(..))
-import qualified Crypto.PubKey.Ed25519 as Ed25519
 import Disciplina.DB.DSL.Types (QueryTx(..), QueryTxs(..), WHERE(..)
                                ,TxIdEq(..), TxGrade(..), QueryObj(..)
                                ,TxsFilterExpr(..), ObjHashEq(..))
@@ -15,14 +15,12 @@ import Disciplina.Educator.Txs (PrivateTxId(..), PrivateTx(..), EducatorTxMsg(..
 import Disciplina.Crypto (Hash, hash, PublicKey, SecretKey)
 import Disciplina.Core (Address(..), CourseId(..), mkAddr, AssignmentId, StudentId)
 import Disciplina.DB.DSL.Interpret (MonadSearchTxObj(..), RunQuery(..))
+import Data.List (union)
+
+import qualified Crypto.PubKey.Ed25519 as Ed25519
 import qualified Disciplina.Core as Core (Grade(..), SubjectId(..))
 import qualified Data.ByteString.Char8 as C
-import Codec.Serialise (Serialise(..))
-import Control.Lens (filtered, traversed)
 
-
-import Crypto.Hash.Algorithms (Blake2sp_256(..))
-import Disciplina.Crypto.Hash.Cryptonite (CryptoniteFunc (..))
 
 -- | Simple transaction database
 newtype SimpleTxDB a = SimpleTxDB
@@ -31,8 +29,8 @@ newtype SimpleTxDB a = SimpleTxDB
 
 instance MonadSearchTxObj SimpleTxDB where
   runTxQuery = runSimpleTxQuery
-  runTxsQuery = undefined
-  runObjQuery = undefined
+  runTxsQuery = runSimpleTxsQuery
+  runObjQuery = undefined -- TODO, implement this one, but how are Object defined?
 
 instance Serialise Address where
   encode (Address h) = encode h
@@ -46,23 +44,48 @@ instance Serialise PrivateTxPayload
 instance Serialise CourseId
 instance Serialise PrivateTx
 
--- | interpreter for SELECTTx WHERE TxIdEq
-runSimpleTxQuery :: QueryTx -> SimpleTxDB (Maybe PrivateTx)
-runSimpleTxQuery (SELECTTx _ (TxIdEq (h :: PrivateTxId)))
-  = do
-     -- find Tx in db with hash h
-     txs <- ask
-     return $ txs ^? traversed . filtered ((==h).hash)
 
--- | query
--- given <tx_hash>, I want to be able to get { <tx> | hash(<tx>) = <tx_hash> }
-findTx :: PrivateTx -> SimpleTxDB (Maybe PrivateTx)
-findTx tx = runQuery q
-  where q = SELECTTx WHERE (TxIdEq (hash tx))
+-- | interpreter for SELECTTx
+runSimpleTxQuery :: QueryTx -> SimpleTxDB (Maybe PrivateTx)
+runSimpleTxQuery (SELECTTx _ (TxIdEq (h :: PrivateTxId))) = do
+  -- find Tx in db with hash == h
+  txs <- ask
+  return $ txs ^? traversed . filtered ((==h).hash)
+
+
+-- | interpreter for SELECTTxs
+runSimpleTxsQuery :: QueryTxs -> SimpleTxDB [PrivateTx]
+runSimpleTxsQuery (SELECTTxs _ (TxSubjectIdEq (sId :: Core.SubjectId))) =
+  -- find Txs in db with SubjectId == a
+  filter ((==sId).ciSubject._ptxCourseId) <$> ask
+
+-- | Find Txs in db with grade == g
+runSimpleTxsQuery (SELECTTxs _ (TxGradeEq grade)) =
+  filter ((== Just grade).getGrade._ptxPayload) <$> ask
+  where getGrade (EducatorTx (GradeCourse g)) = Just g
+        getGrade _ = Nothing
+
+-- | Find Txs in db with grade >= g
+runSimpleTxsQuery (SELECTTxs _ (_ :>= grade)) = do
+  filter ((>= Just grade).getGrade._ptxPayload) <$> ask
+  where getGrade (EducatorTx (GradeCourse g)) = Just g
+        getGrade _ = Nothing
+
+runSimpleTxsQuery (SELECTTxs _ (a :& b)) =
+   union <$> runQuery (SELECTTxs WHERE a) <*> runQuery (SELECTTxs WHERE b)
+
+runSimpleTxsQuery (SELECTTxs _ (a :|| b)) =
+   (<>) <$> runQuery (SELECTTxs WHERE a) <*> runQuery (SELECTTxs WHERE b)
+
+-- TODO, implement this one
+runSimpleTxsQuery (SELECTTxs _ (TxSubjectIsDescendantOf a)) = return []
+
 
 -- | run query
+-- given <tx_hash>, I want to be able to get { <tx> | hash(<tx>) = <tx_hash> }
 runFindTx :: PrivateTx -> Maybe PrivateTx
-runFindTx tx = runReader (getSimpleTxDB $ findTx tx) simpleTxDB
+runFindTx tx = runReader (getSimpleTxDB . runQuery $ q tx) simpleTxDB
+  where q tx = SELECTTx WHERE (TxIdEq (hash tx))
 
 -- | Should return Nothing since mkPrivateTx 'a' 'l'
 -- | does not exist in privateTx db
@@ -72,6 +95,28 @@ testFindTx1 = runFindTx $ mkPrivateTx 'a' 'l'
 -- | Should return Just (mkPrivate 'a' 'k')
 testFindTx2 :: Maybe PrivateTx
 testFindTx2 = runFindTx $ mkPrivateTx 'a' 'k'
+
+
+-- | run query
+-- | I want to be able to get { [<tx>] | P(<ordered_tx_param>) }, where P is a system of (in)equalities
+runFindTxs :: QueryTxs -> [PrivateTx]
+runFindTxs q = runReader (getSimpleTxDB . runQuery $ q) simpleTxDB
+
+txsQuery1 :: QueryTxs
+txsQuery1 = SELECTTxs WHERE (TxSubjectIdEq 1)
+
+txsQuery2 :: QueryTxs
+txsQuery2 = SELECTTxs WHERE (TxSubjectIdEq 2)
+
+txsQuery3 :: QueryTxs
+txsQuery3 = SELECTTxs WHERE (TxGrade :>= Core.B)
+
+txsQuery4 :: QueryTxs
+txsQuery4 = SELECTTxs WHERE (TxSubjectIdEq 2 :& TxGrade :>= Core.B)
+
+txsQuery5 :: QueryTxs
+txsQuery5 = SELECTTxs WHERE (TxSubjectIdEq 2 :& TxGrade :>= Core.A)
+
 
 
 simpleTxDB :: [PrivateTx]
@@ -86,17 +131,18 @@ mkPrivateTx studentKey educatorKey = PrivateTx {
        _ptxStudentId = mkAddr . fst $ mkKeyPair studentKey
      , _ptxCourseId = courseId
      , _ptxEducatorId = mkAddr . fst $ mkKeyPair educatorKey
-     , _ptxPayload = payload
+     , _ptxPayload = payload2
      }
  where
    courseId = CourseId { ciSubject = 1
                        , ciId = 2
                        }
-   payload = StudentTx { _ptxStudentMsg  = Enroll }
+   payload1 = StudentTx { _ptxStudentMsg  = Enroll }
+   payload2 = EducatorTx { _ptxEducatorMsg  = GradeCourse Core.B }
 
 -- | create key pair from seed
 mkKeyPair :: Char -> (PublicKey, SecretKey)
 mkKeyPair seed =
   let (CryptoPassed x) = Ed25519.secretKey (C.replicate 32 seed)
-  in (AbstractPK (Ed25519.toPublic x) :: PublicKey, AbstractSK x :: SecretKey)
+  in (AbstractPK (Ed25519.toPublic x), AbstractSK x)
 
