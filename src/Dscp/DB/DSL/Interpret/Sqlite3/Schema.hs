05ad5befe98a825224acbdb2e6b328a5b8af71a5
Kir <hindmost.one@gmail.com>
2018-06-30 21:07:59 +0400

    [DSCP-109] Squash later

--- src/Dscp/DB/DSL/Interpret/Sqlite3/Schema.hs
+++ src/Dscp/DB/DSL/Interpret/Sqlite3/Schema.hs
@@ -11,7 +11,4 @@ import Dscp.DB.DSL.Interpret.Sqlite3.FileQuoter (qFile)
 
 schema :: IsString s => s
 schema =
-    -- Spaces between quasiquote brackets and file name actually break lookup.
-    -- DO NOT believe documentation example at:
-    -- http://hackage.haskell.org/package/template-haskell-2.7.0.0/docs/Language-Haskell-TH-Quote.html
     [qFile|./database/schema.sql|]
