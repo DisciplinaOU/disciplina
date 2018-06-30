b8a5381dd69d8e4eaf33ae51d72fab3257553f4e
Kir <hindmost.one@gmail.com>
2018-06-30 22:22:09 +0400

    [DSCP-109] Merging `Schema` and `SchemaInit` modules into first one

--- src/Dscp/DB/SQLite.hs
+++ src/Dscp/DB/SQLite.hs
@@ -1,8 +1,8 @@
 
 module Dscp.DB.SQLite (module M) where
 
-import Dscp.DB.SQLite.Class      as M
-import Dscp.DB.SQLite.Functions  as M
-import Dscp.DB.SQLite.Types      as M
-import Dscp.DB.SQLite.Instances  as M ()
-import Dscp.DB.SQLite.SchemaInit as M
+import Dscp.DB.SQLite.Class     as M
+import Dscp.DB.SQLite.Functions as M
+import Dscp.DB.SQLite.Types     as M
+import Dscp.DB.SQLite.Instances as M ()
+import Dscp.DB.SQLite.Schema    as M
