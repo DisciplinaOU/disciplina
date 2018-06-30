17eb830ba38a173e53d3a048c233b878838a213a
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-06-30 19:11:19 +0300

    ci: backport appendBuildFlag

--- default.nix
+++ default.nix
@@ -2,10 +2,15 @@ let
   overlay = import ''${builtins.fetchGit "ssh://git@github.com:/serokell/serokell-overlay.git"}/pkgs'';
   nixpkgs = import (builtins.fetchTarball "https://github.com/serokell/nixpkgs/archive/master.tar.gz") {
     overlays = [ overlay ];
-  };
+  };  
 in
 
 with nixpkgs;
+with haskell.lib;
+
+let
+  appendBuildFlag = drv: x: overrideCabal drv (drv: { buildFlags = (drv.buildFlags or []) ++ [x]; });
+in
 
 buildStackApplication {
   package = "disciplina";
@@ -14,7 +19,7 @@ buildStackApplication {
 
   overrides = final: previous: {
     rocksdb-haskell = dependCabal previous.rocksdb-haskell [ rocksdb ];
-    cardano-sl-networking = haskell.lib.appendConfigureFlag previous.cardano-sl-networking "--ghc-option=-fno-warn-redundant-constraints";
-    disciplina = (haskell.lib.appendBuildFlag previous.disciplina "-Werror").overrideAttrs (super: { preConfigure = "${final.hpack}/bin/hpack ."; });
+    cardano-sl-networking = appendConfigureFlag previous.cardano-sl-networking "--ghc-option=-fno-warn-redundant-constraints";
+    disciplina = (appendBuildFlag previous.disciplina "-Werror").overrideAttrs (super: { preConfigure = "${final.hpack}/bin/hpack ."; });
   };
 }
