d946ef5c751ed7d19ea7340f73f0d056c4967631
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2018-06-30 19:51:42 +0300

    [DSCP-119] Tests for encryption primitives

---
+++ tests/Test/Dscp/Crypto/Encrypt.hs
@@ -0,0 +1,21 @@
+module Test.Dscp.Crypto.Encrypt where
+
+import qualified Data.ByteString as BS
+import Test.Common
+
+import Dscp.Crypto (Encrypted, PassPhrase, decrypt, encrypt, minPassPhraseLength, mkPassPhrase)
+
+instance Arbitrary PassPhrase where
+    arbitrary = either error identity . mkPassPhrase <$>
+        arbitrary `suchThat` ((>= minPassPhraseLength) . BS.length)
+
+spec_encryption :: Spec
+spec_encryption = describe "AES encryption functions" $ do
+    it "should stay the same after encryption and decryption" $ property $
+        \(pp, bs :: ByteString) -> decrypt pp (encrypt pp bs) === Just bs
+    it "should yield different ciphertexts for different plaintexts" $ property $
+        \(pp, bs :: ByteString, bs' :: ByteString) -> bs /= bs' ==> encrypt pp bs /= encrypt pp bs'
+    it "should encrypt differently for different passphrases" $ property $
+        \(pp, pp', bs :: ByteString) -> pp /= pp' ==> encrypt pp bs /= encrypt pp' bs
+    it "should fail when trying to decrypt a ciphertext with wrong passphrase" $ property $
+        \(pp, pp', bs :: ByteString) -> decrypt pp (encrypt pp' bs) === Nothing
