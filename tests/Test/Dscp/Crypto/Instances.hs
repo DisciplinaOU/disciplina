13ef9f9a7ab2c7fd3de6b16d715370196be5dd09
martoon <martoon_00@mail.ru>
2018-07-04 17:22:10 +0300

    [DSCP-122] Fix tests

--- tests/Test/Dscp/Crypto/Instances.hs
+++ tests/Test/Dscp/Crypto/Instances.hs
@@ -2,8 +2,8 @@ module Test.Dscp.Crypto.Instances () where
 
 import Test.Common
 
-import Dscp.Crypto (AbstractSK, EncryptSafe, Encrypted, FromByteArray, PassPhrase, SignatureScheme,
-                    encrypt, genSecretKey, mkPassPhrase)
+import Dscp.Crypto (AbstractSK, Encrypted, FromByteArray, PassPhrase, SignatureScheme, encrypt,
+                    genSecretKey, mkPassPhrase)
 
 instance (SignatureScheme ss, FromByteArray (AbstractSK ss)) =>
          Arbitrary (AbstractSK ss) where
@@ -12,6 +12,6 @@ instance (SignatureScheme ss, FromByteArray (AbstractSK ss)) =>
 instance Arbitrary PassPhrase where
     arbitrary = arbitrary `suchThatMap` (rightToMaybe . mkPassPhrase)
 
-instance (Arbitrary ba, FromByteArray ba, EncryptSafe ba) =>
+instance (Arbitrary ba, FromByteArray ba) =>
          Arbitrary (Encrypted ba) where
     arbitrary = encrypt <$> arbitrary <*> arbitrary
