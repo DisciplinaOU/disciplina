3b745ddca0baf0759c8fb2e8b52ec60669959c95
martoon <martoon_00@mail.ru>
2018-07-05 15:54:26 +0300

    [DSCP-123] Simple launcher script
    Added launcher scripts for both educator and witness nodes.
    
    Layout of files used by nodes is also changed to keep everything
    in single place.

---
+++ scripts/launch/node.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+# directory with script
+base=$(dirname "$0")
+# project root
+root="$base/../.."
+
+# permanent read-only files: configs, secrets...
+files="$root/run"
+# gitignored files: databases...
+tmp_files="$files/tmp/"
+
+mkdir $tmp_files 2> /dev/null || true
+
+##################
+# Parsing params
+##################
+
+for var in "$@"
+do
+    if [[ "$var" == "witness" ]] || [ "$var" == "w" ]; then
+        node="witness"
+    elif [[ "$var" == "educator" ]] || [ "$var" == "e" ]; then
+        node="educator"
+    else
+        echo "Unknown parameter \"$var\""
+        exit 1
+    fi
+done
+
+##################
+# Launch
+##################
+
+# educator-only params
+educator_params="
+--keyfile-password 12345678
+--keyfile-path $files/educator.key
+--sql-path $tmp_files/educator.db
+"
+
+# witness params (and educator's as well)
+witness_params="
+--bind 127.0.0.1:4010:4011
+--db-path $tmp_files/witness.db
+--log-dir $tmp_files/logs
+"
+
+if [[ "$node" == "educator" ]]; then
+    stack exec "disciplina-educator" -- $witness_params $educator_params
+elif [[ "$node" == "witness" ]]; then
+    stack exec "disciplina-witness" -- $witness_params
+elif [[ "$node" == "" ]]; then
+    echo "Please specify which node to run (e.g. \"educator\" or just \"e\")"
+    exit 1
+else
+    echo "Unknown node type \"$node\""
+    exit 1
+fi
