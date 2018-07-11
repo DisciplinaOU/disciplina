b73ed8a2c24fc42f759ad3f69379f26f5f2c51b3
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2018-07-11 22:40:40 +0300

    [DSCP-146] `FromHttpApiData` instances

--- src/Dscp/Educator/Web/Student/Types.hs
+++ src/Dscp/Educator/Web/Student/Types.hs
@@ -13,12 +13,15 @@ module Dscp.Educator.Web.Student.Types
 import Data.Aeson.Options (defaultOptions)
 import Data.Aeson.TH (deriveJSON)
 import Data.Time.Clock (UTCTime)
+import Servant (FromHttpApiData (..))
 
+import Dscp.Core.Address (addrFromText)
 import Dscp.Core.Aeson ()
 import qualified Dscp.Core.Types as Core
 import Dscp.Crypto (Hash, Raw)
 import Dscp.Educator.Txs (PrivateTx (..))
 import Dscp.Educator.Web.Student.Error (APIError)
+import Dscp.Util (fromBase64)
 import Dscp.Util.Aeson (AsByteString, Base64Encoded)
 
 data Course = Course
@@ -71,3 +74,22 @@ deriveJSON defaultOptions ''Submission
 deriveJSON defaultOptions ''Grade
 deriveJSON defaultOptions ''BlkProof
 deriveJSON defaultOptions ''ErrResponse
+
+---------------------------------------------------------------------------
+-- FromHttpApiData instances
+---------------------------------------------------------------------------
+
+deriving instance FromHttpApiData Core.Course
+deriving instance FromHttpApiData Core.Subject
+deriving instance FromHttpApiData Core.Grade
+
+instance FromHttpApiData Core.DocumentType where
+    parseQueryParam "offline" = Right Core.Offline
+    parseQueryParam "online"  = Right Core.Online
+    parseQueryParam other     = Left $ "invalid document type: " <> other
+
+instance FromHttpApiData Core.Address where
+    parseQueryParam = addrFromText
+
+instance FromHttpApiData (Hash a) where
+    parseQueryParam = first toText . fromBase64
