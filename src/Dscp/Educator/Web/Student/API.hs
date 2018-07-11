adf6e24cc82ad665d59b79cd1c6c60c07231c454
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2018-07-11 22:40:07 +0300

    [DSCP-146] Build with new servant (and `allow-newer`)

--- src/Dscp/Educator/Web/Student/API.hs
+++ src/Dscp/Educator/Web/Student/API.hs
@@ -8,8 +8,7 @@ module Dscp.Educator.Web.Student.API
        ) where
 
 import Data.Time.Clock (UTCTime)
-import Servant.API
-import Servant.API.Verbs (StdMethod (..), Verb)
+import Servant
 
 import qualified Dscp.Core.Types as Core
 import Dscp.Crypto (Hash)
@@ -33,15 +32,15 @@ type API
 
 type GetCourses
     = "courses"
-    -- :> Summary "Get Educator's courses"
-    -- :> Description "Gets a list of Educator's courses, both enrolled and available."
+    :> Summary "Get Educator's courses"
+    :> Description "Gets a list of Educator's courses, both enrolled and available."
     :> QueryParam "enrolled" Bool
     :> Verb 'GET 200 '[JSON] [Course]
 
 type GetCourse
-    = "courses" :> Capture "courseId" Int
-    -- :> Summary "Get info about the course"
-    -- :> Description "Gets all info about the given course."
+    = "courses" :> Capture "courseId" Core.Course
+    :> Summary "Get info about the course"
+    :> Description "Gets all info about the given course."
     :> Verb 'GET 200 '[JSON] Course
 
 ---------------------------------------------------------------------------
@@ -50,19 +49,19 @@ type GetCourse
 
 type GetAssignments
     = "assignments"
-    -- :> Summary "Get student's assignments"
-    -- :> Description "Gets a list of student's assignments. Filter parameters are \
-                   -- \used to specify specific course, type, etc."
+    :> Summary "Get student's assignments"
+    :> Description "Gets a list of student's assignments. Filter parameters are \
+                   \used to specify specific course, type, etc."
     :> QueryParam "course" Core.Course
     :> QueryParam "type" Core.DocumentType
     :> QueryParam "final" Bool
     :> Verb 'GET 200 '[JSON] [Assignment]
 
 type GetAssignment
-    = "assignments" :> Capture "assignmentHash" Text
-    -- :> Summary "Get info about an assignment"
-    -- :> Description "Gets an assignment info by given submission hash. Returns \
-                   -- \404 if a student tries to get an assignment which is not assigned to them."
+    = "assignments" :> Capture "assignmentHash" (Hash Core.Assignment)
+    :> Summary "Get info about an assignment"
+    :> Description "Gets an assignment info by given submission hash. Returns \
+                   \404 if a student tries to get an assignment which is not assigned to them."
     :> Verb 'GET 200 '[JSON] Assignment
 
 ---------------------------------------------------------------------------
@@ -71,41 +70,41 @@ type GetAssignment
 
 type GetSubmissions
     = "submissions"
-    -- :> Summary "Get student's submissions"
-    -- :> Description "Gets a list of student's submissions. Filter parameters are \
-                   -- \used to specify specific course, assignment, etc."
+    :> Summary "Get student's submissions"
+    :> Description "Gets a list of student's submissions. Filter parameters are \
+                   \used to specify specific course, assignment, etc."
     :> QueryParam "course" Core.Course
     :> QueryParam "assignment" (Hash Core.Assignment)
     :> QueryParam "type" Text
     :> Verb 'GET 200 '[JSON] [Submission]
 
 type GetSubmission
-    = "submissions" :> Capture "submissionHash" Text
-    -- :> Summary "Get info about a submission"
-    -- :> Description "Gets a submission data by given submission hash. Returns a 404 \
-                   -- \if a student tries to get a submission which is not their own."
+    = "submissions" :> Capture "submissionHash" (Hash Core.Submission)
+    :> Summary "Get info about a submission"
+    :> Description "Gets a submission data by given submission hash. Returns a 404 \
+                   \if a student tries to get a submission which is not their own."
     :> Verb 'GET 200 '[JSON] Submission
 
 type MakeSubmission
     = "submissions"
-    -- :> Summary "Make a new submission"
-    -- :> Description "Posts a new submission with a given body. Request body should \
-                   -- \contain valid student's signature of submission contents, \
-                   -- \otherwise an error will be raised."
+    :> Summary "Make a new submission"
+    :> Description "Posts a new submission with a given body. Request body should \
+                   \contain valid student's signature of submission contents, \
+                   \otherwise an error will be raised."
     :> ReqBody '[JSON] Core.SignedSubmission
     :> Verb 'POST 200 '[JSON] Submission
 
 type DeleteSubmission
-    = "submissions" :> Capture "submissionHash" Text
-    -- :> Summary "Delete a submission"
-    -- :> Description "Deletes a submission from a database. Only ungraded submissions can be deleted."
+    = "submissions" :> Capture "submissionHash" (Hash Core.Submission)
+    :> Summary "Delete a submission"
+    :> Description "Deletes a submission from a database. Only ungraded submissions can be deleted."
     :> Verb 'DELETE 200 '[JSON] ()
 
 type GetProofs
     = "proofs"
-    -- :> Summary "Get available proofs for student"
-    -- :> Description "Gets private transactions together with corresponding Merkle \
-                   -- \subtrees. Transactions from same blocks are grouped together \
-                   -- \and each group has one proof, which is a corresponding Merkle subtree."
+    :> Summary "Get available proofs for student"
+    :> Description "Gets private transactions together with corresponding Merkle \
+                   \subtrees. Transactions from same blocks are grouped together \
+                   \and each group has one proof, which is a corresponding Merkle subtree."
     :> QueryParam "since" UTCTime
     :> Verb 'GET 200 '[JSON] [BlkProof]
