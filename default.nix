3f38d3ea1533eb74917c8ee35c11e5ecf4391c98
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-07-18 16:06:47 +0300

    [DSCP-135] Run hpack earlier on, in src
    Nix builds individual packages in sandboxes that do not have access
    to ../hpack. This runs hpack prior to cabal2nix which alleviates
    the issue.

--- default.nix
+++ default.nix
@@ -3,7 +3,22 @@ with haskell.lib;
 
 buildStackApplication {
   package = "disciplina";
-  src = lib.cleanSource ./.;
+
+  # Running hpack manually before the build is required
+  # because of the problem in stack2nix -- it builds every
+  # subpackage in a separate environment, thus moving
+  # hpack directory out of the scope.
+  #
+  # See: https://github.com/input-output-hk/stack2nix/pull/107
+
+  src = runCommand "source" { cwd = lib.cleanSource ./.; } ''
+    cp --no-preserve=mode,ownership -r $cwd $out
+
+    for f in $out/{lib,tools}; do
+      ${haskellPackages.hpack}/bin/hpack $f
+    done
+  '';
+
   ghc = haskell.compiler.ghc822;
 
   overrides = final: previous: {
@@ -11,7 +26,6 @@ buildStackApplication {
     disciplina = overrideCabal previous.disciplina (super: with final; {
       configureFlags = [ "--ghc-option=-Werror" ];
       doCheck = true;
-      preConfigure = "${hpack}/bin/hpack .";
       testDepends = [ hspec tasty tasty-discover tasty-hspec ];
     });
   };
