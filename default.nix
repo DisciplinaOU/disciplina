fa362b3224ba65b0c35c8e575ef3de8a5f56b557
Yorick <yorickvanpelt@gmail.com>
2018-08-10 11:07:52 +0200

    [OPS-418] hpack faucet package

--- default.nix
+++ default.nix
@@ -25,7 +25,7 @@ buildStackApplication rec {
   src = runCommand "source" { cwd = lib.cleanSource ./.; } ''
     cp --no-preserve=mode,ownership -r $cwd $out
 
-    for f in $out/{core,witness,educator,tools,wallet}; do
+    for f in $out/{core,witness,educator,tools,wallet,faucet}; do
       ${haskellPackages.hpack}/bin/hpack $f
     done
   '';
