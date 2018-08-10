d110832ef081d878d7ac04915a957a2f61d83aff
Mikhail Volkhov <volhovm.cs@gmail.com>
2018-08-10 13:25:30 +0300

    [DSCP-187] Remove unused statestorage in SD.Storage.Pure

--- witness/src/Dscp/Snowdrop/Storage/Pure.hs
+++ witness/src/Dscp/Snowdrop/Storage/Pure.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE RankNTypes          #-}
 
-module Dscp.Snowdrop.Storage.Pure where
+module Dscp.Snowdrop.Storage.Pure
+    ( blockDbActions
+    ) where
 
 import Control.Concurrent.STM.TVar (modifyTVar)
 import Control.Lens (at, makeLenses)
@@ -15,20 +17,12 @@ import Snowdrop.Util
 
 import Dscp.Core.Foundation (HeaderHash)
 import Dscp.Snowdrop.Configuration
-import Dscp.Snowdrop.Types
 
 data BlockStorage = BlockStorage
     { _bsBlunds :: Map HeaderHash SBlund
     , _bsTip    :: TipValue HeaderHash
     }
 makeLenses ''BlockStorage
-
-type AddrMap = Map AccountId Account
-
-data StateStorage = StateStorage
-    { _ssAddrMap        :: AddrMap
-    }
-makeLenses ''StateStorage
 
 emptyBlockStorage :: BlockStorage
 emptyBlockStorage = BlockStorage mempty (TipValue Nothing)
@@ -66,7 +60,7 @@ blockDbActions = mkActions <$> liftIO (newTVarIO emptyBlockStorage)
 
     queryOne :: TVar BlockStorage -> Ids -> STM (Maybe Values)
     queryOne var = \case
-        (TipKeyIds TipKey)          -> Just . TipValueVal . _bsTip <$> readTVar var
+        (TipKeyIds TipKey)          -> Just . TipValueVal . view bsTip <$> readTVar var
         (BlockRefIds (BlockRef hh)) -> fmap BlundVal . M.lookup hh . _bsBlunds <$> readTVar var
         i -> throwM $ DbWrongIdQuery $ "Unknown query to block storage " <> show i
 
@@ -75,7 +69,7 @@ blockDbActions = mkActions <$> liftIO (newTVarIO emptyBlockStorage)
 
     applyOne :: TVar BlockStorage -> Ids -> ValueOp Values -> STM ()
     applyOne var (TipKeyIds TipKey) =
-        \valueop -> unTipValue . _bsTip <$> readTVar var >>= applyTip var valueop
+        \valueop -> unTipValue . view bsTip <$> readTVar var >>= applyTip var valueop
     applyOne var (BlockRefIds (BlockRef hh)) =
         performActionWithTVar var (bsBlunds . at hh) (applyException hh) <=<
         projValOp @(BlockRef HeaderHash)
@@ -86,51 +80,6 @@ blockDbActions = mkActions <$> liftIO (newTVarIO emptyBlockStorage)
     applyTip _   vOp val                      = applyException TipKey vOp val
 
     setTip var newTip = modifyTVar var (\bs -> bs { _bsTip = newTip })
-
-simpleStateDbActions ::
-       (MonadIO m, MonadIO n)
-    => AddrMap
-    -> m (DbModifyActions (SumChangeSet Ids Values) Ids Values n ())
-simpleStateDbActions initAddrMap =
-    mkActions <$> newTVarIO (StateStorage initAddrMap)
-  where
-    mkActions var =
-        DbModifyActions (mkAccessActions var) (atomically . apply var)
-    mkAccessActions var =
-        sumChangeSetDBA (atomically ... queryMany (queryOne var)) (atomically ... iter var)
-
-    -- Queries
-    queryOne :: TVar StateStorage -> Ids -> STM (Maybe Values)
-    queryOne tvar = \case
-        AccountInIds a -> fmap AccountOutVal . M.lookup a . _ssAddrMap <$> readTVar tvar
-        i -> throwM $ DbWrongIdQuery $ "Unknown query to state " <> show i
-
-    iter :: TVar StateStorage -> Prefix -> b -> ((Ids, Values) -> b -> b) -> STM b
-    iter var prefix
-        | prefix == accountPrefix =
-          iterHelper AccountInIds AccountOutVal (_ssAddrMap <$> readTVar var)
-        | otherwise =
-          const $ const $ throwM $
-          DbWrongPrefixIter $ "Unknown iteration on state " <> show prefix
-
-    -- Modifies
-    apply :: TVar StateStorage -> SumChangeSet Ids Values -> STM ()
-    apply vars accum = mapM_ (uncurry $ applyOne vars) $ changeSetToList $ accumToDiff accum
-
-    applyOne :: TVar StateStorage -> Ids -> ValueOp Values -> STM ()
-    applyOne var (AccountInIds a) =
-        performActionWithTVar var (ssAddrMap . at a) (applyException a) <=<
-        projValOp @AccountId
-    applyOne _ i = applyException i '-'
-
-    iterHelper :: (id' -> Ids)
-               -> (val' -> Values)
-               -> STM (Map id' val')
-               -> b
-               -> ((Ids, Values) -> b -> b)
-               -> STM b
-    iterHelper injId injVal var b foldF =
-        foldr foldF b . map (bimap injId injVal) . M.toList <$> var
 
 
 ----------------------------------------------------------------------------
@@ -153,14 +102,6 @@ projValOp
 projValOp valueop = case sequenceA $ proj @Values @(SValue id) <$> valueop of
     Nothing -> throwM $ ValueTypeMismatched valueop (Proxy @value)
     Just v  -> pure v
-
-performActionWithMap ::
-       (Show id, Show value, Ord id)
-    => TVar (Map id value)
-    -> id
-    -> ValueOp value
-    -> STM ()
-performActionWithMap tvar i = performActionWithTVar tvar (at i) (applyException i)
 
 performActionWithTVar
     :: TVar var
