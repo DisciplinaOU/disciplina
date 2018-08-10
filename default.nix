7b821b98316ec2cc5e1e8580e5227535e0f11106
Yorick van Pelt <yorick@yorickvanpelt.nl>
2018-08-10 17:33:43 +0200

    add curl wrapper

--- default.nix
+++ default.nix
@@ -5,7 +5,10 @@ with import (fetchTarball "https://github.com/serokell/nixpkgs/archive/master.ta
   name = "srk-buildkite-scripts";
   src = ./.;
   buildPhase = "true";
+  buildInputs = [ makeWrapper curl ];
   installPhase = ''
     install -Dm755 bin/* -t "$out/bin"
+    wrapProgram $out/bin/deploy-result.sh \
+      --suffix PATH : ${curl}/bin
   '';
 }
