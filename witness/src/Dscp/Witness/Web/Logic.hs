225d1690f2b8b46d0b2ce01258bf927e16bdab87
goodsoft <goodsoft@goodsoft.lv>
2018-08-13 19:26:09 +0300

    [DSCP-174] Throw API errors

--- witness/src/Dscp/Witness/Web/Logic.hs
+++ witness/src/Dscp/Witness/Web/Logic.hs
@@ -11,20 +11,13 @@ import UnliftIO.Async (async)
 
 import Dscp.Core
 import Dscp.Snowdrop
-import Dscp.Util (assertJust)
+import Dscp.Util (assertJust, nothingToThrow)
 import Dscp.Witness.Launcher.Mode (WitnessWorkMode)
 import qualified Dscp.Witness.Relay as Relay
 import Dscp.Witness.Config
 import Dscp.Witness.Logic
 import Dscp.Witness.Web.Error
 import Dscp.Witness.Web.Types
-
-----------------------------------------------------------------------------
--- Errors
-----------------------------------------------------------------------------
-
-noAccount :: WitnessWebError
-noAccount = EntityAbsent "No such address registered"
 
 ----------------------------------------------------------------------------
 -- Logic
@@ -71,19 +64,26 @@ getBlocks :: WitnessWorkMode ctx m => Maybe Int -> Maybe HeaderHash -> m [BlockI
 getBlocks mCount mFrom = do
     let count = max 100 $ fromMaybe 100 mCount
     from <- maybe (runSdMRead getTipHash) return mFrom
+    runSdMRead (getBlockMaybe from) >>= void . nothingToThrow BlockNotFound
     eBlocks <- runSdMRead $ getBlocksBefore count from
     either (throwM . InternalError) (return . map (toBlockInfo False) . toList) eBlocks
 
 getBlockInfo :: WitnessWorkMode ctx m => HeaderHash -> m BlockInfo
-getBlockInfo = runSdMRead . getBlock >=> return . toBlockInfo True
+getBlockInfo =
+    runSdMRead . getBlockMaybe >=>
+    nothingToThrow BlockNotFound >=>
+    return . toBlockInfo True
 
 getAccountInfo :: WitnessWorkMode ctx m => Address -> Bool -> m AccountInfo
 getAccountInfo address includeTxs = do
-    account <- getAccountMaybe address `assertJust` noAccount
+    account <- getAccountMaybe address `assertJust` AccountNotFound
     txs <- if includeTxs
         then Just <$> getAccountTxs address
         else return Nothing
     return $ toAccountInfo account txs
 
 getTransactionInfo :: WitnessWorkMode ctx m => GTxId -> m TxInfo
-getTransactionInfo = runSdMRead . getTx >=> return . toTxInfo
+getTransactionInfo =
+    runSdMRead . getTxMaybe >=>
+    nothingToThrow TransactionNotFound >=>
+    return . toTxInfo
