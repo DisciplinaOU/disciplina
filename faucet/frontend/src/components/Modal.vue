14e5d96b8b31ad8df9a57ab0a78f3ff6d62af173
E.Tankova <chipalinca_ne93_@mail.ru>
2018-08-14 18:42:03 +0300

    select is ready, modal is fixed

--- faucet/frontend/src/components/Modal.vue
+++ faucet/frontend/src/components/Modal.vue
@@ -18,6 +18,9 @@
         <div class="modalContent__hash hash">{{ secretKey }}</div>
         <div class="modalContent__button btn btn--blue-white" :class="{ 'btn--blue-white--active' : secretKeyCopied }" @click="copyKey('secretKey')">{{ $t(secretKeyCopied ? "copied" : "copy") }}</div>
       </div>
+      <div class="modalLayer__content modalContent">
+        <p class="modalContent__prompt">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia <a class="modalContent__link" href="#">deserunt</a> mollit anim id est laborum.</p>
+      </div>
     </div>
   </div>
 </template>
