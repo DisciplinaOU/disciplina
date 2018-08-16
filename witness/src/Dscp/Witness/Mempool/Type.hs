eaef61eb47409686816f249f8189f5658cc55177
Heimdell <hindmost.one@gmail.com>
2018-08-16 09:58:04 +0400

    [DSCP-139] Removing qualified `Conf` import

--- witness/src/Dscp/Witness/Mempool/Type.hs
+++ witness/src/Dscp/Witness/Mempool/Type.hs
@@ -8,19 +8,19 @@ module Dscp.Witness.Mempool.Type
 
 import Control.Lens (makeLenses)
 import Dscp.Core.Foundation (GTxWitnessed)
-import qualified Dscp.Snowdrop.Configuration as Conf
+import qualified Dscp.Snowdrop.Configuration
 import qualified Dscp.Snowdrop.IOCtx as SD
 import qualified Dscp.Snowdrop.Storage.Avlp as AVLP
 import qualified Dscp.Witness.SDLock as Lock
 import qualified Snowdrop.Model.Mempool as Pool
 
-type MempoolVar = MakeMempoolVar (SD.IOCtx (AVLP.AVLChgAccum Conf.Ids Conf.Values))
+type MempoolVar = MakeMempoolVar (SD.IOCtx (AVLP.AVLChgAccum Ids Values))
 
 type MakeMempoolVar ctx = (Mempool ctx, Lock.SDLock)
 
 data Mempool ctx = Mempool
-    { _mpSDPool   :: Pool.Mempool Conf.Ids Conf.Values (AVLP.AVLChgAccum Conf.Ids Conf.Values) GTxWitnessed
-    , _mPSDConfig :: Pool.MempoolConfig Conf.Exceptions Conf.Ids Conf.Proofs Conf.Values ctx GTxWitnessed
+    { _mpSDPool   :: Pool.Mempool Ids Values (AVLP.AVLChgAccum Ids Values) GTxWitnessed
+    , _mPSDConfig :: Pool.MempoolConfig Exceptions Ids Proofs Values ctx GTxWitnessed
     }
 
 makeLenses ''Mempool
