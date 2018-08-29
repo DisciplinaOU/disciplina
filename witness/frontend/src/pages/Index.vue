615fc3a18d026fb1c978636ecddd66f0fdc5586e
E.Tankova <chipalinca_ne93_@mail.ru>
2018-08-29 13:20:16 +0300

    some fixes and new block notFound

--- witness/frontend/src/pages/Index.vue
+++ witness/frontend/src/pages/Index.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="main">
+    <div class="notFound">
+      <div class="container">
+        <h3 class="notFound__title blockTitle marginBottom15 marginTop30">No matching records for:</h3>
+        <p class="notFound__text text-center marginBottom30">DdzFFzCqrhssmgBTif5DRb4NCXbz2FsZfEXXBdknGq1CTvovyXq9ECmx5amfjeWrftYEsayi9BCAahJ8dRgrx</p>
+        <p class="notFound__button text-center marginBottom30">
+          <a class="btn btn--blue-white btn--arrow btn--forward">Return to homepage</a>
+        </p>
+      </div>
+    </div>
     <div class="lastSlots">
       <div class="container">
         <h3 class="lastSlots__title blockTitle marginBottom30 marginTop30">{{ $t("blocks") }}</h3>
