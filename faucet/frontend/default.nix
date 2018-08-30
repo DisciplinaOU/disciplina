d844d10c8dc9f0a42f0b222143e3afba1664f300
Christian Höppner <christian.hoppner@serokell.io>
2018-08-30 22:45:19 +0100

    Clean up derivations

--- faucet/frontend/default.nix
+++ faucet/frontend/default.nix
@@ -1,10 +1,11 @@
-{ pkgs ? (import <nixpkgs> {}), stdenv ? pkgs.stdenv, faucetURL ? "" }:
-with pkgs;
+{ stdenv, faucetUrl ? "", yarn, parallel, brotli }:
 stdenv.mkDerivation {
   name = "disciplina-faucet-frontend";
-  src = lib.cleanSource ./.;
-  buildInputs = [ pkgs.yarn pkgs.parallel pkgs.brotli ];
-  FAUCET_API_URL = faucetURL;
+  src = stdenv.lib.cleanSource ./.;
+  FAUCET_API_URL = faucetUrl;
+  HOME = ".";
+
+  buildInputs = [ yarn parallel brotli ];
   buildPhase = ''
     yarn install
     yarn build
@@ -14,7 +15,9 @@ stdenv.mkDerivation {
       -not -name '*.webp' \
       -not -name '*.woff' \
       -not -name '*.woff2' | parallel brotli
-
   '';
-  installPhase = "cp -R dist/ $out";
+
+  installPhase = ''
+    mv dist $out
+  '';
 }
