be0427634d7f8fe55cba7476cb42808dc297120d
Roman Novoselov <rnovoselov93@gmail.com>
2018-09-03 15:33:04 +0300

    Autoupdate transactions list (each 20 sec.)

--- witness/frontend/src/store/transactions.js
+++ witness/frontend/src/store/transactions.js
@@ -9,9 +9,22 @@ const state = {
 const actions = {
   getAllTransactions ({commit}, from = state.nextTransactionHash, perPage = state.perPage) {
     blockexplorer.getTransactions((nextId, Transactions) => {
+      if (state.nextTransactionHash === undefined) {
+        setAutoupdateList(this)
+      }
       commit('setNextTransactionHash', nextId)
       commit('setTransactions', Transactions)
     }, from, perPage)
+  },
+  fetchNewTransactions ({commit}, from) {
+    blockexplorer.getTransactions((nextId, Transactions) => {
+      for (var i = Transactions.length - 1; i >= 0; i--) {
+        if (Transactions[i].txId === state.transactions[0].txId) {
+          commit('unshiftTransactions', Transactions.slice(0, i))
+          break
+        }
+      }
+    }, undefined, state.perPage)
   }
 }
 
@@ -21,6 +34,9 @@ const mutations = {
   },
   setTransactions (state, transactions) {
     state.transactions.push(...transactions)
+  },
+  unshiftTransactions (state, transactions) {
+    state.transactions.unshift(...transactions)
   }
 }
 
@@ -31,6 +47,12 @@ const getters = {
   transactions (state) {
     return state.transactions
   }
+}
+
+function setAutoupdateList (store) {
+  setInterval(() => {
+    store.dispatch('fetchNewTransactions')
+  }, 20000)
 }
 
 export default {
