8ee1f0e6e19e7e82899f8c186f8f16aac3aa981b
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2018-09-07 04:20:48 +0300

    [DSCP-228] Comment regarding `Given` constraint with vinyl records

--- witness/src/Dscp/Witness/Config.hs
+++ witness/src/Dscp/Witness/Config.hs
@@ -13,14 +13,18 @@ module Dscp.Witness.Config
 import Data.Reflection (Given)
 import Loot.Config (ConfigKind (Final, Partial), ConfigRec)
 
+import Dscp.Config
 import Dscp.Core.Config
-
 
 type WitnessConfig = CoreConfig
 
 type WitnessConfigRecP = ConfigRec 'Partial WitnessConfig
 type WitnessConfigRec = ConfigRec 'Final WitnessConfig
 
+-- TODO: `HasWitnessConfig` does not imply `HasCoreConfig`,
+-- even though `CoreConfig` is included into `WitnessConfig`.
+-- We need some more advanced constraint, something like
+-- @type GivenSub a = forall b. (b <: a, Given b)
 type HasWitnessConfig = Given WitnessConfigRec
 
 withWitnessConfig :: WitnessConfigRec -> (HasWitnessConfig => a) -> a
