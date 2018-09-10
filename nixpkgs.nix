53b401297b1d0e2d7729ad18b19c1f32e560aa6d
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-09-10 10:00:19 +0000

    Pin Nixpkgs, do not depend on serokell-overlay

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,8 @@
 let
-  nixpkgs = fetchTarball "https://github.com/serokell/nixpkgs/archive/master.tar.gz";
-  serokell-overlay = fetchGit "ssh://git@github.com:/serokell/serokell-overlay";
+  nixpkgs = fetchGit {
+    url = "https://github.com/serokell/nixpkgs";
+    rev = "4089219f345ce7bd0c3bd579d9190e526ea50b52";
+  };
 in
 
-import nixpkgs { overlays = [ (import "${serokell-overlay}/pkgs") ]; }
+import nixpkgs {}
