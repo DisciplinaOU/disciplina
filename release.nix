5e674d3c01fcfd2c0e24b1a2ee5402c3b89dda0a
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-09-10 10:01:43 +0000

    Add disciplina-wallet-macos-sandbox drv, add writeShellScript

--- release.nix
+++ release.nix
@@ -12,24 +12,39 @@ let
   }) {};
   
   project = import ./. { inherit pkgs; };
+
+  writeShellScript = source: writeTextFile {
+    name = "script";
+    executable = true;
+    checkPhase = "${shellcheck} $out";
+    text = ''
+      #!/bin/sh
+      ${source}
+    '';
+  };
 in
 
 rec {
   disciplina-wallet = haskell.lib.justStaticExecutables project.disciplina-wallet;
 
-  disciplina-wallet-wrapped = writeShellScriptBin "disciplina-wallet" ''
+  disciplina-wallet-wrapped = writeShellScript ''
     ${disciplina-wallet}/bin/dscp-wallet --witness https://witness.disciplina.io
   '';
 
   disciplina-wallet-flatpak = buildFlatpak {
     app-id = "io.disciplina.Wallet";
-    command = "${disciplina-wallet-wrapped}/bin/disciplina-wallet";
+    command = disciplina-wallet-wrapped;
     finish-args = [ "--share=network" ];
   };
 
+  disciplina-wallet-macos-sandbox = writeShellScript ''
+    sandbox-exec -D HOME="$HOME" -D DYLD_ROOT_PATH="$DYLD_ROOT_PATH" \
+      -f ${./wallet/profile.sb} ${disciplina-wallet-wrapped}/bin/disciplina-wallet
+  '';
+
   disciplina-wallet-macos-app = buildMacOSApp {
     name = "Disciplina";
-    target = "${disciplina-wallet-wrapped}/bin/disciplina-wallet";
+    target = disciplina-wallet-macos-sandbox;
     withOpen = true;
   };
 }
