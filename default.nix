1cf28d0b1f1e8d14fcf1c30b8fcfc557500f4c8c
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-09-17 13:35:19 +0000

    Switch to serokell-closure

--- default.nix
+++ default.nix
@@ -1,28 +1,27 @@
-{ pkgs ? import ./nixpkgs.nix }: with pkgs;
+{ pkgs ? import ./closure.nix }: with pkgs;
 
 let 
-  stack4nix = fetchGit {
-    url = "https://github.com/serokell/stack4nix";
-    rev = "26a46991270749ecc57d5842d391435b314dc26f";
-  };
-
-  filterWhiteBlack = { path, whitelist, blacklist ? [] }: name: type: let
-    relPath = (lib.removePrefix (toString path + "/") name) + (if type == "directory" then "/" else "");
-    reMatch = (re: builtins.match re relPath != null);
-  in
+  filterWhiteBlack = { path, whitelist, blacklist ? [] }: name: type:
+    let
+      relPath = (lib.removePrefix (toString path + "/") name) + (if type == "directory" then "/" else "");
+      reMatch = (re: builtins.match re relPath != null);
+    in
     (lib.any reMatch whitelist) && !(lib.any reMatch blacklist);
+
   composeFilters = a: b: name: type: (a name type) && (b name type);
 
-  buildStackProject = import stack4nix { inherit pkgs; };
-  disciplinaPackages = buildStackProject (builtins.path rec {
+  # TODO: fix this in stack-to-nix instead
+  src = builtins.path rec {
     path = ./.;
     name = "disciplina";
     filter = composeFilters (filterWhiteBlack {
       inherit path;
       whitelist = [ "stack\.yaml" ".*/.*" ];
       blacklist = [ ".*node_modules.*" "docs.*" "run.*" "scripts.*" "secrets.*" "specs.*" ];
-    }) (lib.cleanSourceFilter);
-  });
+    }) lib.cleanSourceFilter;
+  };
+
+  disciplinaPackages = stackToNix { inherit src; };
 in
 
 disciplinaPackages // rec {
