efb95b4e8630b89a632665a8703efaa062a09bd9
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-09-20 11:49:25 +0000

    Generate spec PDFs on CI

---
+++ specs/default.nix
@@ -0,0 +1,70 @@
+{ pkgs ? import ../closure.nix }: with pkgs;
+
+let
+  latex = texlive.combine {
+    inherit (texlive)
+      scheme-small
+      biber
+      bbm
+      bbm-macros
+      collection-langcyrillic
+      csquotes
+      tabu
+      varwidth
+      floatrow
+      fontawesome
+      footmisc
+      algorithms
+      algorithmicx
+      enumitem
+      biblatex
+      biblatex-gost
+      logreq
+      xstring
+      lastpage
+      lipsum
+      preprint
+      totcount
+      chngcntr
+      titlesec
+      todonotes
+      paratype
+      was
+      filecontents
+      multirow
+      cm-super
+      subfigure
+      appendixnumberbeamer
+      ucs
+      siunitx;
+  };
+in
+
+stdenv.mkDerivation {
+  name = "disciplina-specs";
+  src = lib.cleanSource ./src;
+
+  nativeBuildInputs = [
+    latex
+    pdftk
+    ghostscript
+    biber
+    graphviz
+  ];
+
+  buildPhase = ''
+    export HOME=$PWD
+
+    for d in */; do
+      (cd $d && pdflatex main.tex)
+    done
+  '';
+
+  installPhase = ''
+    mkdir $out
+
+    for f in */main.pdf; do
+      mv $f $out/$(dirname $f).pdf
+    done
+  '';
+}
