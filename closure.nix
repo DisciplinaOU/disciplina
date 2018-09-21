8081651b392b36a4f0be91e50664c4281a571192
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-09-21 14:22:41 +0000

    Update Serokell closure

--- closure.nix
+++ closure.nix
@@ -1,4 +1,4 @@
 import (fetchGit {
   url = https://github.com/serokell/serokell-closure;
-  rev = "d781f98b2111025afdeab0d179ca08c892537a13";
+  rev = "2773aaebd95e772c8c738bdaaa2342153e068372";
 })
