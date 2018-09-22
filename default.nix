187ec20fed359a61a4c66f6538fdf39bcb997f79
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-09-22 20:26:47 +0000

    Remind to add proper source filtering to stack-to-nix

--- default.nix
+++ default.nix
@@ -1,12 +1,16 @@
 { pkgs ? import ./closure.nix, shell ? false }: with pkgs;
 
 stackToNix {
+  # TODO: properly fix this in stack-to-nix
   root = gitIgnore ./. [
     "*.md"
     "*.nix"
     "*.tex"
     ".buildkite"
     "config.yaml"
+    "scripts"
+    "secrets"
+    "specs"
   ];
   inherit shell;
 }
