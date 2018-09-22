090bdc98645a79107c4f258e4f94d68ee65f4395
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-09-22 23:58:52 +0000

    Implement Stack shell

--- closure.nix
+++ closure.nix
@@ -1,4 +1,4 @@
 import (fetchGit {
   url = https://github.com/serokell/serokell-closure;
-  rev = "34980db0d470dddf2afc503c4c2d8197b0760e52";
+  rev = "89a0592f4af2562e1f45e51b6e5c98d104e334ce";
 })
