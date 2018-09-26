3f054290230c4c69d185d788aab813b9d3890a44
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-09-26 16:18:31 +0000

    Bump closure (unique Stack snapshot names)

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchGit {
   url = https://github.com/serokell/serokell-closure;
-  rev = "89a0592f4af2562e1f45e51b6e5c98d104e334ce";
+  rev = "53c441e21059bf54c31481a0968473ccc5a73e29";
 })
