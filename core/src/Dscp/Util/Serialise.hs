94085b63c3dc36c002a95632b5502890d51052a8
martoon <martoon.hsk@gmail.com>
2018-09-26 13:05:09 +0300

    Add Remainder

--- core/src/Dscp/Util/Serialise.hs
+++ core/src/Dscp/Util/Serialise.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# OPTIONS_GHC -Wno-partial-type-signatures #-}
+{-# LANGUAGE RecursiveDo           #-}
 
 -- | Utility functions for binary serialisation
 
@@ -10,17 +11,21 @@ module Dscp.Util.Serialise
        , encodeCrcProtected
        , decodeCrcProtected
 
+       , encodeWithRemainder
+       , decodeRemainder
+
        , Mem1 (..)
        , Mem2 (..)
        , example
        ) where
 
-import Codec.CBOR.Term (decodeTerm)
+import Codec.CBOR.Term (Term (TNull), decodeTerm, encodeTerm)
 import Codec.Serialise (DeserialiseFailure, Serialise (..), deserialise, deserialiseOrFail,
                         serialise)
 import qualified Codec.Serialise.Decoding as D
 import qualified Codec.Serialise.Encoding as E
 import qualified Data.ByteString.Lazy as LBS
+import Data.Default (Default (..))
 import Data.Digest.CRC32 (crc32)
 import Data.HList.FakePrelude
 import Data.HList.HList
@@ -72,13 +77,78 @@ As previous, we encode complex objects as lists, but now we are lenient to
 the case when list contains more entries than we expect (when it describes
 object from the future version). In some sense, encoded list length stands for
 the version of the object of given type.
-To tolerate all future versions in decoding logic at once without manipulating
-with lengths manually, 'HList' is proposed to be used as intermediate structure.
 
 Disadvantage of this approach is that we can only extend a datatype, replacing
 or removing fields is not possible without breaking compatibility.
 
 -}
+
+-- | Redundant terms which came from data of higher version, we don't know what
+-- to do with them, but we want to keep them to later evaluate
+-- forward-compatible hashes and signatures.
+--
+-- It is isomorphic to 'ByteString', but we keep it as terms because
+-- "serialise" library will hardly work with actual ByteStrings.
+newtype Remainder = Remainder [Term]
+    deriving (Show, Eq, Ord)
+
+instance Default Remainder where
+    def = Remainder []
+
+-- | Encode given complex data with remainder. You need only to 'encode'
+-- your datatype fields, length of list representing the type is filled for you.
+encodeWithRemainder :: Word -> Remainder -> E.Encoding -> E.Encoding
+encodeWithRemainder mainTermsN (Remainder terms) mainEncoder =
+    E.encodeListLen (mainTermsN + remainderSize) <>
+    mainEncoder <>
+    mconcat (map encodeTerm terms)
+  where
+    remainderSize = fromIntegral $ length terms :: Word
+
+-- | Decode given complex data with remainder.
+-- You have to provide number of decoded fields and previously decoded
+-- length of list which represents the datatype.
+decodeRemainder :: Word -> Int -> D.Decoder s Remainder
+decodeRemainder (fromIntegral -> mainTermsN) totalN = mdo
+    unless (totalN >= mainTermsN) $
+        fail $ "Length of list representing object (which?) is too small, \
+               \expected at least" <> show mainTermsN <> ", but got " <>
+               show totalN
+
+    remainder <- replicateM (totalN - mainTermsN) decodeTerm
+    return $ Remainder $ force remainder
+
+instance NFData Term where
+    rnf = \case
+        TNull -> ()
+        _     -> ()
+
+---------------------------------------------------------------------------
+-- How serialisation would look like
+---------------------------------------------------------------------------
+
+data Ololo = Ololo Int Double Remainder
+
+instance Serialise Ololo where
+    encode (Ololo i d rema) =
+        encodeWithRemainder 2 rema $
+            encode i <>
+            encode d
+    decode = do
+        len <- D.decodeListLen
+        i <- decode
+        d <- decode
+        rema <- decodeRemainder 2 len
+        return $ Ololo i d rema
+
+-- For more cute serialisation instances and demostration of achieved
+-- forward-compatibility see below
+
+---------------------------------------------------------------------------
+-- Removing boilerplate
+---------------------------------------------------------------------------
+
+{- If the following looks scaring, jump to examples below first -}
 
 -- | Used to do @map encode@ on elements of 'HList'.
 data HEncode = HEncode
@@ -86,31 +156,37 @@ instance (e ~ E.Encoding, Serialise a) => ApplyAB HEncode a e where
     applyAB _ x = encode x
 
 -- | Encode given 'HList'.
-encodeHList :: _ => HList l -> E.Encoding
-encodeHList l =
-    let len = hNat2Integral (hLength l)
-        content = mconcat $ hList2List $ hMap HEncode l
-    in E.encodeListLen len <> content
+encodeH :: _ => Remainder -> HList l -> E.Encoding
+encodeH rema l =
+    encodeWithRemainder contentPartLen rema $
+        mconcat $ hList2List (hMap HEncode l)
+  where
+    contentPartLen = hNat2Integral (hLength l)
 
 -- | Used to decode 'HList'.
 class HDecodeLenient l where
     -- | Decodes given number of arguments into given elements.
     -- Number of arguments should be equal or larger than expected list size,
     -- extra entries would be throw away.
-    hDecode :: Int -> D.Decoder s (HList l)
+    hDecode :: Int -> D.Decoder s (HList l, Remainder)
 
 instance HDecodeLenient '[] where
-    hDecode n = HNil <$ replicateM_ n decodeTerm
+    hDecode n = do
+        rema <- replicateM n decodeTerm
+        return (HNil, Remainder $ force rema)
 
 instance (Serialise x, HDecodeLenient xs) => HDecodeLenient (x : xs) where
     hDecode n = do
         x <- decode
-        xs <- hDecode (n - 1)
-        return (x .*. xs)
+        (xs, rema) <- hDecode (n - 1)
+        return (x .*. xs, rema)
 
 -- | Decode elements to 'HList'. Extra terms will be droped.
-decodeHListLenient :: forall l n s. (HLengthEq l n, _) => D.Decoder s (HList l)
-decodeHListLenient = do
+decodeHLenient
+    :: forall l n s.
+       (HLengthEq l n, _)
+    => D.Decoder s (HList l, Remainder)
+decodeHLenient = do
     let expectedLen = hNat2Integral (Proxy @n)
     len <- D.decodeListLen
     when (len < expectedLen) $
@@ -131,40 +207,40 @@ instance TupleHList (a, b, c) (HList '[a, b, c]) where
 -- Example
 ---------------------------------------------------------------------------
 
--- | First version of some type.
-data Mem1 = Mem1 Int Double
-    deriving (Show, Eq)
+-- | First version of some type, we take place for future extensions.
+data Mem1 = Mem1 Int Double Remainder
+    deriving (Show, Eq, Generic)
 
 instance Serialise Mem1 where
-    encode (Mem1 i d) = encodeHList $ hBuild i d
+    encode (Mem1 i d rema) = encodeH rema $ hBuild i d
     decode = do
-        (i, d) <- fromHList <$> decodeHListLenient
-        return $ Mem1 i d
+        ((i, d), rema) <- first fromHList <$> decodeHLenient
+        return $ Mem1 i d rema
 
 -- | Second (extended) version of the type.
-data Mem2 = Mem2 Int Double Text
+data Mem2 = Mem2 Int Double Text Remainder
     deriving (Show, Eq)
 
 instance Serialise Mem2 where
-    encode (Mem2 i d t) = encodeHList $ hBuild i d t
+    encode (Mem2 i d t rema) = encodeH rema $ hBuild i d t
     decode = do
-        (i, d, t) <- fromHList <$> decodeHListLenient
-        return $ Mem2 i d t
+        ((i, d, t), rema) <- first fromHList <$> decodeHLenient
+        return $ Mem2 i d t rema
 
 example :: IO ()
 example = do
-    let m1 = Mem1 1 2
+    let m1 = Mem1 1 2 def
     -- This is obviously valid
     print $ deserialise (serialise m1) == m1
     -- >>> True
 
-    -- Later we extended that datatype (it has another name for simplicity)
-    let m2 = Mem2 1 2 "sobaka"
+    -- Later we extended that datatype (it has another name for unambiguity)
+    let m2 = Mem2 1 2 "sobaka" def
     print $ deserialise (serialise m2) == m2
     -- >>> True
 
     -- And this will still work:
     print $ deserialise (serialise m2) == m1
-    -- >>> True
+    -- >>> False, because remainder differs, but deserialisation succeedes
 
     return ()
