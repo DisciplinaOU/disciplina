6bb754981162e273fefa7b01ce72544bacb90105
Kirill Andreev <hindmost.one@gmail.com>
2018-12-17 20:16:08 +0400

    More saving

--- tools/src/faircv/Client.hs
+++ tools/src/faircv/Client.hs
@@ -1,50 +1,46 @@
 module Client where
 
--- import Data.Traversable (for)
-
--- import Dscp.Core
--- import Dscp.Crypto
--- import Dscp.Educator.Web.Student
--- import Dscp.Educator.Web.Types
--- import Dscp.Util
--- import Dscp.Util.Aeson
-
--- import Test.QuickCheck
-
--- data InvalidProofs = InvalidProofs deriving (Show, Exception)
-
--- -- | Get all proofs since given time.
--- getProofs :: StudentApiClient -> IO [MerkleProof PrivateTx]
--- getProofs sc = do
---     rawProofs <- sGetProofs sc Nothing False
---     let zipProof BlkProofInfo {..} = mergeProofAndData
---             (unEncodeSerialised bpiMtreeSerialized)
---             bpiTxs
---     nothingToThrow InvalidProofs $
---         mapM zipProof rawProofs
-
--- getAssignments :: StudentApiClient -> IO [AssignmentStudentInfo]
--- getAssignments sc = do
---     hashes <- map aiHash <$> sGetAssignments sc Nothing Nothing Nothing False
---     for hashes $ sGetAssignment sc
-
--- makeRandomSubmissionForAssignment :: StudentApiClient -> Hash Assignment -> IO NewSubmission
--- makeRandomSubmissionForAssignment sc aHash = do
---     return NewSubmission
---         { nsAssignmentHash = aHash
---         , nsContentsHash   = hash "mkay"
---         , nsWitness        = _
---         }
-
--- getAllCourses :: StudentApiClient -> IO [Course]
--- getAllCourses sc = do
---     map ciId <$> sGetCourses sc Nothing False
-
+import Data.Traversable (for)
 
 import Dscp.Core
+import Dscp.Crypto
+import Dscp.Educator.Web.Student
+import Dscp.Educator.Web.Types
 import Dscp.Witness.Web.Client
+import Dscp.Util
+import Dscp.Util.Aeson
 
 import Test.QuickCheck
+
+data InvalidProofs = InvalidProofs deriving (Show, Exception)
+
+-- | Get all proofs since given time.
+getProofs :: StudentApiClient -> IO [MerkleProof PrivateTx]
+getProofs sc = do
+    rawProofs <- sGetProofs sc Nothing False
+    let zipProof BlkProofInfo {..} = mergeProofAndData
+            (unEncodeSerialised bpiMtreeSerialized)
+            bpiTxs
+    nothingToThrow InvalidProofs $
+        mapM zipProof rawProofs
+
+getAssignments :: StudentApiClient -> IO [AssignmentStudentInfo]
+getAssignments sc = do
+    hashes <- map aiHash <$> sGetAssignments sc Nothing Nothing Nothing False
+    for hashes $ sGetAssignment sc
+
+makeRandomSubmissionForAssignment :: StudentApiClient -> Hash Assignment -> IO NewSubmission
+makeRandomSubmissionForAssignment sc aHash = do
+    return NewSubmission
+        { nsAssignmentHash = aHash
+        , nsContentsHash   = hash "mkay"
+        , nsWitness        = _
+        }
+
+getAllCourses :: StudentApiClient -> IO [Course]
+getAllCourses sc = do
+    map ciId <$> sGetCourses sc Nothing False
+
 
 getFairCV :: IO FairCV
 getFairCV = generate arbitrary
--- tools/src/faircv/Main.hs
+++ tools/src/faircv/Main.hs
@@ -1,13 +1,18 @@
 
 import Servant.Client
 
-import Dscp.Witness.Web.Client
+import Dscp.Educator.Web.Student.Client.Logic
+-- import Dscp.Witness.Web.Client
 
 import Client
 
 main :: IO ()
 main = do
-    wClient <- createWitnessClient =<< parseBaseUrl "127.0.0.1"
-    fairCV  <- getFairCV
-    res     <- checkFairCV wClient fairCV
-    print res
+    -- wClient <- createWitnessClient =<< parseBaseUrl "127.0.0.1:8013"
+    -- fairCV  <- getFairCV
+    -- res     <- checkFairCV wClient fairCV
+    -- print res
+
+    sClient <- createStudentApiClient =<< parseBaseUrl "127.0.0.1:8090"
+    asses   <- getAssignments sClient
+    print asses
