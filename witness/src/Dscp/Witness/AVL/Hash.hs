7f4c85b68a747ee2c248101b90a92ba3884aca83
Kirill Andreev <hindmost.one@gmail.com>
2018-12-24 15:04:26 +0400

    [DSCP-411] Ported to the newest AVL+

--- witness/src/Dscp/Witness/AVL/Hash.hs
+++ witness/src/Dscp/Witness/AVL/Hash.hs
@@ -4,11 +4,10 @@ module Dscp.Witness.AVL.Hash
     ( AvlHash (..)
     ) where
 
-import Codec.Serialise (Serialise (..))
-import qualified Codec.Serialise as S
+import Codec.Serialise (Serialise (..), serialise)
 import Control.Monad.Free (Free (..))
-import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Tree.AVL as AVL
+import qualified Data.ByteString.Lazy as BSL
 import Snowdrop.Execution ()
 
 import Dscp.Crypto (Hash, unsafeHash)
@@ -17,13 +16,19 @@ import Dscp.Crypto (Hash, unsafeHash)
 newtype AvlHash = AvlHash { unAvlHash :: Hash () } deriving (Eq, Ord, Show, Generic)
 instance Serialise AvlHash
 
-instance (Serialise (f (Free f a)), Serialise a) => Serialise (Free f a)
+instance {-# OVERLAPPING #-} (Serialise h, Serialise k, Serialise v) => Serialise (AVL.Map h k v) where
+    encode = encode . AVL.beforeSerialise
+    decode = AVL.afterDeserialise <$> decode
 
-instance Serialise AVL.Tilt
-instance Serialise b => Serialise (AVL.WithBounds b)
-instance (Serialise h, Serialise k, Serialise v, Serialise r) => Serialise (AVL.MapLayer h k v r)
+instance (Serialise a, Serialise (f (Free f a))) => Serialise (Free f a)
 
-instance (Serialise k, Serialise v) => AVL.Hash AvlHash k v where
-    hashOf =
-        AvlHash .
-        (unsafeHash :: ByteString -> Hash ()) . BSL.toStrict . S.serialise
+-- instance Serialise AVL.Tilt
+-- instance Serialise b => Serialise (AVL.WithBounds b)
+instance (Serialise h, Serialise k, Serialise v, Serialise r, Serialise t) => Serialise (AVL.MapLayerTemplate t h k v r)
+
+instance Serialise x => AVL.ProvidesHash x AvlHash where
+    getHash
+        = AvlHash
+        . (unsafeHash :: ByteString -> Hash ())
+        . BSL.toStrict
+        . serialise
