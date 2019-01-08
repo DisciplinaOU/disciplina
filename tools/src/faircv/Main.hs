adef9bb3b3d3671e7e586a89b267be7b891840a7
pasqu4le <pasqu4le@gmail.com>
2019-01-08 16:05:47 +0100

    [DSCP-431] use student API in faircv tool

--- tools/src/faircv/Main.hs
+++ tools/src/faircv/Main.hs
@@ -1,23 +1,34 @@
 
 import Data.Aeson
-import qualified Data.Set as Set
-import Data.Time.Clock
-import Data.Traversable
 import Servant.Client
 import Test.QuickCheck
+import Time.Units (threadDelay, sec)
 
 import Loot.Log
 
 import Dscp.Core
 import Dscp.Crypto
+import Dscp.Educator.Web.Student
 import Dscp.Resource.Keys
+import Dscp.Util (leftToFail)
+import Dscp.Util.Test (detGenG)
 import Dscp.Witness.Web hiding (checkFairCV)
 
 import Client
 
+-- This is a copy of dscp-keygen's parseInputWithSecret (for SecretFromSeed)
+secretFromSeed :: ByteString -> Maybe SecretKey
+secretFromSeed input = asum [
+      do
+        sd <- readMaybe @Word . toString @Text $ decodeUtf8 input
+        return $ withIntSeed (fromIntegral sd) genSecretKey
+    , do
+        return $ withSeed input genSecretKey
+    ]
+
 createActor :: IO (SecretKey, PublicKey, Address)
 createActor = do
-    sk   <- genSecretKey
+    Just sk   <- return $ secretFromSeed ("456" :: ByteString)
     pk   <- return (toPublic sk)
     addr <- return (mkAddr   pk)
     return (sk, pk, addr)
@@ -35,74 +46,50 @@ instance MonadLogging IO where
 
 main :: IO ()
 main = do
-    [url, skFile] <- getArgs
+    [wUrl, eUrl, skFile] <- getArgs
+    -- create the APIs clients
+    wClient <- createWitnessClient =<< parseBaseUrl wUrl
+    sClient <- createStudentApiClient =<< parseBaseUrl eUrl
 
-    wClient <- createWitnessClient =<< parseBaseUrl url
-
+    -- read educator's secret key from the file
     store <- readStore skFile emptyPassPhrase
-
     sk <- return $ store^.krSecretKey
 
-    (pk,  addr)       <- createActorOf sk
+    -- make keys and addresses for educator and students
+    (_pk,  addr)      <- createActorOf sk
     (skS, pkS, addrS) <- createActor
 
-    privateTxs <- for [1..10 :: Integer] $ \_ -> do
-        timestamp      <- toTimestamp <$> getCurrentTime
-        contentsHash   <- generate arbitrary
-        assignmentHash <- generate arbitrary
-
-        let sub = Submission
-                  { _sStudentId      = addrS
-                  , _sContentsHash   = contentsHash
-                  , _sAssignmentHash = assignmentHash
-                  }
-
-        return PrivateTx
-            { _ptGrade            = gA
-            , _ptTime             = timestamp
-            , _ptSignedSubmission = SignedSubmission
-                { _ssSubmission = sub
-                , _ssWitness    = SubmissionWitness
-                    { _swKey = pkS
-                    , _swSig = sign skS (hash sub)
-                    }
+    let seed = "47295" :: Text -- NOTE: this can be randomized, or user defined
+        infoToSubmission :: AssignmentStudentInfo -> Submission
+        infoToSubmission assInfo = Submission
+            { _sStudentId      = addrS
+            , _sContentsHash   = detGenG seed arbitrary
+            , _sAssignmentHash = aiHash assInfo
+            }
+        toSignedSubmission :: Submission -> SignedSubmission
+        toSignedSubmission sbm = SignedSubmission
+            { _ssSubmission = sbm
+            , _ssWitness    = SubmissionWitness 
+                { _swKey = pkS
+                , _swSig = sign skS $ hash sbm
                 }
             }
+        infoToNewSub :: AssignmentStudentInfo -> NewSubmission
+        infoToNewSub = signedSubmissionToRequest . toSignedSubmission . infoToSubmission
 
-    tree <- return $ fromFoldable privateTxs
-    sig  <- return $ getMerkleRoot tree
+    -- Get all the available assignments for this student
+    assLst <- sGetAssignments sClient Nothing Nothing Nothing False
+    -- Make a new submission from the first one, then send it
+    mapM_ (sendSubmission sClient . infoToNewSub) $ take 1 assLst
 
-    hdr <- return PrivateBlockHeader
-        { _pbhPrevBlock = genesisHeaderHash addr
-        , _pbhBodyProof = sig
-        , _pbhAtgDelta  = mempty
-        }
-
-    ptx <- return PublicationTx
-        { ptAuthor     = addr
-        , ptFeesAmount = unsafeMkCoin (100 :: Integer)
-        , ptHeader     = hdr
-        }
-
-    res <- wSubmitPublication wClient PublicationTxWitnessed
-        { ptwTx = ptx
-        , ptwWitness = PublicationTxWitness
-            { pwSig = sign sk (toPtxId ptx, pk, hdr)
-            , pwPk  = pk
-            }
-        }
-
-    print res
-
-    Just proof <- return $ mkMerkleProof tree (Set.fromList [0..9])
-
-    fcv :: FairCV <- return $ unReadyFairCV $
-        singletonFCV addrS "John Doe" addr (hash hdr) (readyProof proof)
+    -- Wait some time for the proofs to be available, then get them and make a faircv
+    logInfo "Submissions sent, waiting for proofs"
+    threadDelay $ sec 20
+    proofs <- sGetProofs sClient Nothing False
+    
+    fcvrs <- mapM (blkToFairCV addrS "John Doe" addr) proofs
+    fcv <- unReadyFairCV <$> leftToFail (mergeFairCVList fcvrs)
 
     writeFile "fairCV-example.json" $ decodeUtf8 $ encode fcv
 
     print =<< checkFairCV wClient fcv
-
-    -- sClient <- createStudentApiClient =<< parseBaseUrl "127.0.0.1:8090"
-    -- asses   <- getAssignments sClient
-    -- print asses
