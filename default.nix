6d25a93832ccd11a7fbb6fe19e18f8cebdff2ef7
Yorick van Pelt <yorick@yorickvanpelt.nl>
2019-01-15 17:11:37 +0100

    [OPS-413] [OPS-697] use pg_tmp while running disciplina educator tests

--- default.nix
+++ default.nix
@@ -1,5 +1,18 @@
 { pkgs ? import ./pkgs.nix, shell ? false }: with pkgs;
-
+let
+  withPostgreSQL = drv: pkgs.haskell.lib.overrideCabal drv (a: {
+    testToolDepends = (a.testToolDepends or []) ++
+      [ pkgs.ephemeralpg pkgs.postgresql pkgs.getopt ];
+    preCheck = (a.preCheck or "") + ''
+      export TEST_PG_CONN_STRING=$(pg_tmp -w 600)
+    '';
+    # we need the temporary directory from pg_tmp
+    # so extract it out of $TEST_PG_CONN_STRING
+    postCheck = (a.postCheck or "") + ''
+      pg_tmp stop -d $(echo ''${TEST_PG_CONN_STRING#*=} | sed 's:%2F:/:g' || :)
+    '';
+  });
+in
 stackToNix {
   # TODO: properly fix this in stack-to-nix
   root = constGitIgnore "disciplina-src" ./. [
@@ -13,4 +26,7 @@ stackToNix {
     "/specs"
   ];
   inherit shell;
+  overrides = final: previous: {
+    disciplina-educator = withPostgreSQL previous.disciplina-educator;
+  };
 }
